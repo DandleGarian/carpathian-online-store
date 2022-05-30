0d47197f9aac823399aec7ee74f67e6bf0e3d418
DandleGarian <daniel.dulgerian@gmail.com>
2022-05-30 15:56:07 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./**/*.{liquid,json}"
+  ],
+  prefix: 'tw-',
+  theme: {
+    screens: {
+      'md': '750px',
+      'lg': '990px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
